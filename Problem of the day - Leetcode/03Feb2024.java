ce736ef30492c455184f2fceb2c054082cc82442
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2024-02-04 09:45:57 +0530

    Create 03Feb2024.java

---
+++ Problem of the day - Leetcode/03Feb2024.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int maxVal = 0;
+            for (int j = 1; j <= k && (i - j) >= 0; j++) {
+                maxVal = Math.max(maxVal, arr[i - j]);
+                dp[i] = Math.max(dp[i], dp[i - j] + maxVal * j);
+            }
+        }
+
+        return dp[n];
+    }
+}
