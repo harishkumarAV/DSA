7a0a13e3867ce07ef1d23e5d60d5fb35cfe565f1
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2024-03-30 09:06:17 +0530

    Create MinimumElementInBST.java

---
+++ Trees/MinimumElementInBST.java
@@ -0,0 +1,25 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Solution {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node root) {
+        // code here
+        if(root == null){
+            return Integer.MAX_VALUE;
+        }
+        while(root.left!=null){
+            root = root.left;
+        }
+        return root.data;
+    }
+}
