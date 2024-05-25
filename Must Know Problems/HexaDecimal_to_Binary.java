ff608604688a6100763189489212c516b004364d
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2024-05-25 19:07:30 +0530

    Create HexaDecimal_to_Binary.java

---
+++ Must Know Problems/HexaDecimal_to_Binary.java
@@ -0,0 +1,11 @@
+
+import java.io.*;
+class HexToBinary {
+	public static void main(String[] args)
+	{
+		String s = "1AC5";
+		int decimalValue = Integer.parseInt(input, 16);
+		String result = Integer.toBinaryString(decimalValue);
+    System.out.println(result);
+	}
+}
