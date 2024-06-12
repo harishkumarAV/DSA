eb4caee6efe6037c42fd8c898c6e37e24d819807
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2024-06-12 12:31:06 +0530

    Create MooreVotingAlgorithm.java

---
+++ MooreVotingAlgorithm.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        for(int i=0;i<nums.length;i++){
+            if(count == 0){
+                candidate = nums[i];
+            }
+            if(candidate == nums[i]){
+                count++;
+            }else{
+                count--;
+            }        
+        }
+        return candidate;
+    }
+}
