aacd219f894a590d4ecbe3aa4495cb132e50b522
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2024-06-24 11:52:32 +0530

    Create BestTimeToBuyAndSellStocksiii.java

---
+++ Dynamic Programming/BestTimeToBuyAndSellStocksiii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length==0){
+            return 0;
+        }
+
+        int fb = Integer.MIN_VALUE;
+        int sb = Integer.MIN_VALUE;
+        int fs=0,ss=0;
+
+        for(int i=0;i<prices.length;i++){
+            fb = Math.max(fb,-prices[i]);
+            fs = Math.max(fs, fb + prices[i]);
+            sb = Math.max(sb , fs - prices[i]);
+            ss = Math.max(ss , sb + prices[i]);
+        }
+
+        return ss;
+    }
+}
